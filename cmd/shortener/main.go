bf69ab372e4c7a32a29f026a82f8b14aa37360d3
argad <argad9292@gmail.com>
2025-06-05 15:31:55 +0300

    Theme phase 1

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,103 @@
 package main
 
-func main() {}
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+)
+
+const (
+	addr = ":8080"
+	host = "http://localhost:8080/"
+)
+
+var urlStore = make(map[string]string)
+
+func generateID() string {
+
+	//TODO: если ключ уже есть, то брать старый
+	b := make([]byte, 8)
+	_, err := rand.Read(b)
+	if err != nil {
+		panic(err)
+	}
+
+	return base64.RawURLEncoding.EncodeToString(b)[:8]
+}
+
+// POST create shortener /
+func handleShorten(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost || r.Header.Get("Content-Type") != "text/plain" {
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		return
+	}
+
+	body, err := io.ReadAll(r.Body)
+	if err != nil || len(body) == 0 {
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		return
+	}
+	url := strings.TrimSpace(string(body))
+	if !strings.HasPrefix(url, "http") {
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		return
+	}
+
+	id := generateID()
+	urlStore[id] = url
+
+	shortURL := host + id
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusCreated)
+	_, _ = w.Write([]byte(shortURL))
+}
+
+// GET /{id}
+func handleGetUrl(w http.ResponseWriter, r *http.Request, id string) {
+	//if r.Method != http.MethodGet || r.Header.Get("Content-Type") != "text/plain" {
+	//	http.Error(w, "Bad Request", http.StatusBadRequest)
+	//	return
+	//}
+	fmt.Println("Имя:", id)
+
+	url, success := urlStore[id]
+
+	if !success {
+		http.Error(w, "Bad Request Not Found", http.StatusBadRequest)
+		return
+	}
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusTemporaryRedirect)
+	w.Write([]byte(url))
+}
+
+func handleRequest(w http.ResponseWriter, r *http.Request) {
+	path := r.URL.Path
+
+	if path == "/" {
+		handleShorten(w, r)
+		return
+	}
+
+	if strings.HasPrefix(path, "/") && len(path) > 1 {
+		variable := strings.TrimPrefix(path, "/")
+		handleGetUrl(w, r, variable)
+		return
+	}
+
+	// Если ничего не подошло, возвращаем 404
+	http.Error(w, "Bad Request", http.StatusBadRequest)
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", handleRequest)
+
+	err := http.ListenAndServe(`:8080`, mux)
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/argad/url-shortener
+
+go 1.24.3
