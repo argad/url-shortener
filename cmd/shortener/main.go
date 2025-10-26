eef94eab00f7e4eb409b2e9a7a93eab55da322a9
argad <argad9292@gmail.com>
2025-10-26 16:38:02 +0300

    20251026 - Url shortener phase 21 (start)

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"errors"
+	"log"
+	"net/http"
+
 	"github.com/argad/url-shortener/internal/config"
 	"github.com/argad/url-shortener/internal/factory"
 	"github.com/argad/url-shortener/internal/server"
 	"go.uber.org/zap"
-	"log"
-	"net/http"
+	"golang.org/x/crypto/acme/autocert"
 )
 
 func main() {
@@ -36,8 +39,53 @@ func main() {
 		log.Fatalf("Failed to create new server: %v", err)
 	}
 
-	err2 := http.ListenAndServe(cfg.ServerAddress, srv.Router)
-	if err2 != nil {
-		panic(err2)
+	if cfg.EnableHTTPS {
+		startHTTPSServer(cfg, srv)
+	} else {
+		startHTTPServer(cfg, srv)
+	}
+
+	//err2 := http.ListenAndServe(cfg.ServerAddress, srv.Router)
+	//if err2 != nil {
+	//	panic(err2)
+	//}
+}
+
+func startHTTPServer(cfg *config.Config, srv *server.Server) {
+	log.Printf("Starting HTTP server on %s", cfg.ServerAddress)
+
+	// Create the HTTP server
+	httpServer := &http.Server{
+		Addr:    cfg.ServerAddress,
+		Handler: srv.Router,
+	}
+
+	// Start HTTP server
+	if err := httpServer.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		log.Fatalf("Failed to start HTTP server: %v", err)
+	}
+
+}
+
+func startHTTPSServer(cfg *config.Config, srv *server.Server) {
+	log.Printf("Starting HTTPS server with autocert on %s for domain %s", cfg.ServerAddress, cfg.AutocertDomain)
+
+	// Create autocert manager
+	m := &autocert.Manager{
+		Cache:      autocert.DirCache(cfg.AutocertDir),
+		Prompt:     autocert.AcceptTOS,
+		HostPolicy: autocert.HostWhitelist(cfg.AutocertDomain),
+	}
+
+	// Create the HTTPS server
+	httpsServer := &http.Server{
+		Addr:      cfg.ServerAddress,
+		Handler:   srv.Router,
+		TLSConfig: m.TLSConfig(),
+	}
+
+	// Start HTTPS server
+	if err := httpsServer.ListenAndServeTLS("", ""); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		log.Fatalf("Failed to start HTTPS server: %v", err)
 	}
 }
--- go.mod
+++ go.mod
@@ -23,8 +23,9 @@ require (
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rogpeppe/go-internal v1.14.1 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
-	golang.org/x/crypto v0.37.0 // indirect
-	golang.org/x/sync v0.13.0 // indirect
-	golang.org/x/text v0.24.0 // indirect
+	golang.org/x/crypto v0.43.0 // indirect
+	golang.org/x/net v0.45.0 // indirect
+	golang.org/x/sync v0.17.0 // indirect
+	golang.org/x/text v0.30.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
